de5ba0790c60dc684ee88b4e04d8cdea0018c67d
Adnan <adnanfajri@gmail.com>
2017-07-18 04:06:04 +0700

    edit faq

--- faq.php
+++ faq.php
@@ -9,66 +9,50 @@
 			<!-- Content -->
 			<article id="content">
 				<header>
-					<h2>No Sidebar</h2>
-					<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit dolor neque semper.</p>
+					<h2>Frequently Asked Questions</h2>
 				</header>
-				<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
-				<p>Ut sed tortor luctus, gravida nibh eget, volutpat odio. Proin rhoncus, sapien mollis luctus hendrerit, orci dui viverra
-					metus, et cursus nulla mi sed elit. Vestibulum condimentum, mauris a mattis vestibulum, urna mauris cursus lorem, eu
-					fringilla lacus ante non est. Nullam vitae feugiat libero, eu consequat sem. Proin tincidunt neque eros. Duis faucibus
-					blandit ligula, mollis commodo risus sodales at. Sed rutrum et turpis vel blandit. Nullam ornare congue massa, at commodo
-					nunc venenatis varius. Praesent mollis nisi at vestibulum aliquet. Sed sagittis congue urna ac consectetur.</p>
-				<p>Mauris eleifend eleifend felis aliquet ornare. Vestibulum porta velit at elementum gravida nibh eget, volutpat odio.
-					Proin rhoncus, sapien mollis luctus hendrerit, orci dui viverra metus, et cursus nulla mi sed elit. Vestibulum condimentum,
-					mauris a mattis vestibulum, urna mauris cursus lorem, eu fringilla lacus ante non est. Nullam vitae feugiat libero,
-					eu consequat sem. Proin tincidunt neque eros. Duis faucibus blandit ligula, mollis commodo risus sodales at. Sed rutrum
-					et turpis vel blandit. Nullam ornare congue massa, at commodo nunc venenatis varius. Praesent mollis nisi at vestibulum
-					aliquet. Sed sagittis congue urna ac consectetur.</p>
-				<p>Vestibulum pellentesque posuere lorem non aliquam. Mauris eleifend eleifend felis aliquet ornare. Vestibulum porta velit
-					at elementum elementum.</p>
-			</article>
+				<h3>1. Q: Kapan tanggal pelaksanaan acara KMHE 2017?</h3>
+				<p style="padding-left: 35px">A: 1-4 November 2016</p>
 
-			<div class="row features">
-				<section class="4u 12u(narrower) feature">
-					<div class="image-wrapper first">
-						<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-					</div>
-					<header>
-						<h3>Dolor sit consequat magna</h3>
-					</header>
-					<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur vel sem sit dolor neque semper magna lorem
-						ipsum.</p>
-					<ul class="actions">
-						<li><a href="#" class="button">Elevate my awareness</a></li>
-					</ul>
-				</section>
-				<section class="4u 12u(narrower) feature">
-					<div class="image-wrapper">
-						<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-					</div>
-					<header>
-						<h3>Dolor sit consequat magna</h3>
-					</header>
-					<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur vel sem sit dolor neque semper magna lorem
-						ipsum.</p>
-					<ul class="actions">
-						<li><a href="#" class="button">Elevate my awareness</a></li>
-					</ul>
-				</section>
-				<section class="4u 12u(narrower) feature">
-					<div class="image-wrapper">
-						<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-					</div>
-					<header>
-						<h3>Dolor sit consequat magna</h3>
-					</header>
-					<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur vel sem sit dolor neque semper magna lorem
-						ipsum.</p>
-					<ul class="actions">
-						<li><a href="#" class="button">Elevate my awareness</a></li>
-					</ul>
-				</section>
-			</div>
+				<h3>2. Q: Kapan masa untuk pendaftaran tim?</h3>
+				<p style="padding-left: 35px">A: Pendaftaran dibuka mulai 27 Juni 2016 pukul 13:30 WIB dan ditutup tanggal 25 Juli 2016.</p>
+				
+				<h3>3. Q: Apa alamat email pendaftaran KMHE 2017?</h3>
+				<p style="padding-left: 35px">A: Pendaftaran dikirim ke email ...</p>
+
+				<h3>4. Q: Apakah regulasi kompetisi sudah dirilis?</h3>
+				<p style="padding-left: 35px">A: Regulasi Teknis sudah bisa diunduh melalui web resmi KMHE 2016. Regulasi Non-Teknis akan segera menyusul.</p>
+
+				<h3>5. Q: Siapa narahubung yang dapat dihubungi untuk masalah Teknis, Non-Teknis, dan Pendaftaran/Administrasi?</h3>
+				<p style="padding-left: 35px">A: Informasi lengkap mengenai kontak yang bisa dihubungi dapat dilihat di web resmi KMHE 2017 di bagian <a href="kontak.php">KONTAK</a>. Peserta dihimbau untuk mengajukan pertanyaan sesuai dengan kategori masing-masing contact person (Dosen, Teknis, Non Teknis, Pendaftaran).</p>
+				
+				<h3>6. Q: Bagaimana kalau anggota tim tidak full 8 orang?</h3>
+				<p style="padding-left: 35px">A: Bisa. Tapi wajib ada dosen pembimbing, manajer, driver utama dan driver cadangan.</p>
+
+				<h3>7. Q: Bagaimana kalau KTM salah satu anggota hilang?</h3>
+				<p style="padding-left: 35px">A: Bisa diganti dengan KTM sementara atau surat pengganti KTM sesuai universitas masing-masing.</p>
+
+				<h3>8. Q: Apakah boleh kalau manajer merangkap menjadi driver?</h3>
+				<p style="padding-left: 35px">A: Boleh. Pada bagian nama manajer bisa ditulis nama_driver utama/cadangan. Dan pada kolom driver utama/cadangan bisa ditulis nama anggota yang lain.</p>
+
+				<h3>9. Q: Bagaimana cara mengisi surat keikutsertaan dan surat keterangan dosen?</h3>
+				<p style="padding-left: 35px">A: Untuk surat keikutsertaan pada bagian biodata diisi biodata Purek/Warek/Puket/Pudir Bidang Kemahasiswaan. Untuk surat keterangan dosen pada bagian biodata diisi biodata dosen pembimbin.</p>
+
+				<h3>10. Q: Apakah tim dapat mengganti anggotanya?</h3>
+				<p style="padding-left: 45px">A: Bisa. Caranya dengan mengirimkan Format 1A-1F dan scan kartu tanda mahasiswa anggota tim yang baru.</p>
+
+				<h3>11. Q: Bagaimana cara mengganti anggota tim?</h3>
+				<p style="padding-left: 45px">
+					A: Caranya : 
+					<br>
+					1) Mengisi format 1A-1F sesuai tim yang baru<br>
+					2) Scan KTM anggota tim yang baru<br>
+					3) Kirimkan berkas di atas ke email ... dengan subjek “Pergantian Anggota Tim-Nama Tim-Universitas 
+					<br>
+					*Jika akan melakukan pergantian anggota saat registrasi ulang, dapat langsung dilampirkan dengan berkas daftar ulang tersebut. 
+				</p>
+				
+			</article>
 		</div>
 	</div>
 
