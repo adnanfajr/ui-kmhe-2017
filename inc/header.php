ae098a00bc8f72772bd863f90025f2e9480e74b0
Adnan <adnanfajri@gmail.com>
2017-07-19 01:43:50 +0700

    add jadwal page

--- inc/header.php
+++ inc/header.php
@@ -16,6 +16,7 @@
 		<meta charset="utf-8" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/timeline.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 	</head>
---
+++ jadwal.php
@@ -0,0 +1,68 @@
+<?php
+	require 'inc/header.php';
+	require 'inc/nav.php';
+?>
+    <!-- Main -->
+    <div class="wrapper">
+        <div class="container">
+            <header class="major">
+				<h2>Jadwal Kompetisi</h2>
+			</header>
+            <div class="container-fluid">
+                <div class="row">
+
+                    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+                        <ul class="timeline timeline-split">
+                            <li class="timeline-item">
+                                <div class="timeline-info">
+                                    <span>March 12, 2016</span>
+                                </div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h3 class="timeline-title">Event Title</h3>
+                                </div>
+                            </li>
+                            <li class="timeline-item">
+                                <div class="timeline-info">
+                                    <span>March 23, 2016</span>
+                                </div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h3 class="timeline-title">Event Title</h3>
+                                </div>
+                            </li>
+                            <li class="timeline-item period">
+                                <div class="timeline-info"></div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h2 class="timeline-title">April 2016</h2>
+                                </div>
+                            </li>
+                            <li class="timeline-item">
+                                <div class="timeline-info">
+                                    <span>April 02, 2016</span>
+                                </div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h3 class="timeline-title">Event Title</h3>
+                                </div>
+                            </li>
+                            <li class="timeline-item">
+                                <div class="timeline-info">
+                                    <span>April 28, 2016</span>
+                                </div>
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <h3 class="timeline-title">Event Title</h3>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+	require 'inc/footer.php';
+?>
